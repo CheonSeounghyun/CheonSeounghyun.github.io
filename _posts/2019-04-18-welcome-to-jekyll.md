6496458547b0e335612463a832ede8b9b375f1ba
ehwjsx@gmail.com <ehwjsx@gmail.com>
2023-12-05 13:46:46 +0900

    blog change

--- _posts/2019-04-18-welcome-to-jekyll.md
+++ _posts/2019-04-18-welcome-to-jekyll.md
@@ -1,9 +1,12 @@
 ---
+layout: post
+date: 2019-04-18
 title: "Welcome to Jekyll!"
-date: 2019-04-18T15:34:30-04:00
-categories:
-  - spring
-tags:
+published: true
+lang: ko
+excerpt: Jekyll post example
+tags: spring
+author: seounghyun
 ---
 
 You'll find this post in your `_posts` directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run `jekyll serve`, which launches a web server and auto-regenerates your site when a file is updated.
---
+++ tag_generator.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+'''
+tag_generator.py
+Copyright 2017 Long Qian
+Contact: lqian8@jhu.edu
+This script creates tags for your Jekyll blog hosted by Github page.
+No plugins required.
+'''
+
+import glob
+import os
+import os.path
+
+def tag_generator(post_dir, tag_dir):
+    filenames = glob.glob(post_dir + '*md')
+
+    total_tags = []
+    for filename in filenames:
+        print(f'filename: {filename}')
+        f = open(filename, 'r')
+        crawl = False
+        for line in f:
+            if crawl:
+                current_tags = line.strip().split()
+                if current_tags[0] == 'tags:':
+                    total_tags.extend(current_tags[1:])
+                    crawl = False
+                    break
+            if line.strip() == '---':
+                if not crawl:
+                    crawl = True
+                else:
+                    crawl = False
+                    break
+        f.close()
+    total_tags = set(total_tags)
+
+    old_tags = glob.glob(tag_dir + '*.md')
+    for tag in old_tags:
+        os.remove(tag)
+        
+    if not os.path.exists(tag_dir):
+        os.makedirs(tag_dir)
+
+    for tag in total_tags:
+        tag_filename = tag_dir + tag.lower() + '.md'
+        if os.path.isfile(tag_filename):
+            print(f"{tag} 대소문자 중복")
+            exit(1)
+        f = open(tag_filename, 'a')
+        write_str = f'---\nlayout: tagpage\ntitle: \"Tag: {tag}\"\ntag: {tag}\nrobots: noindex\n---\n'
+        f.write(write_str)
+        f.close()
+
+    return len(total_tags)
+
+
+if __name__ == '__main__':
+    total_tag_count = tag_generator('_posts/', 'tag/')
+    print(f'Tags generated, count {total_tag_count}')
